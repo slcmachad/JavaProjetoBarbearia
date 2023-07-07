5ee2fbe1c499121fdd544cfc30ce4e4fefc3e911
Salomao <salomao.machado.75@gmail.com>
2023-07-06 23:14:17 -0300

    <build> Pessoa: Criação de classe pessoa para utilizar de herança e evitar repetição de codigo.

---
+++ src/model/Pessoa.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.util.Date;
+
+public class Pessoa {
+    
+    protected int id;
+    protected String nome;
+    protected char sexo;
+    protected Date nascimento;
+    protected String telefone;
+    protected String email;
+    protected String rg;
+
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Pessoa(int id, String nome, char sexo, Date nascimento, String telefone, String email, String rg) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.nascimento = nascimento;
+        this.telefone = telefone;
+        this.email = email;
+        this.rg = rg;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    
+    
+}
