b4a0741aac90d94f82d6455fc4f9eca82df756e3
Salomao <salomao.machado.75@gmail.com>
2023-07-08 03:03:54 -0300

    <build> helper: Criação de helper para fazer a logica que será nescessaria para exibir a tabela

--- src/controler/AgendaControler.java
+++ src/controler/AgendaControler.java
@@ -1,21 +1,26 @@
 package controler;
 
 import barbershop.View.Agenda;
+import controler.Helper.AgendaHelper;
 import java.util.ArrayList;
 import model.Agendamento;
 import model.DAO.AgendamentoDAO;
 
 public class AgendaControler {
     private final Agenda view;
+    private final AgendaHelper helper;
 
     public AgendaControler(Agenda view) {
         this.view = view;
+        this.helper = new AgendaHelper(view);
     }
     
     public void atualizaTabela(){
         
         AgendamentoDAO agendamentoDAO = new AgendamentoDAO();
         ArrayList<Agendamento> agendamentos = agendamentoDAO.selectAll();
+        
+        helper.preencherTabela(agendamentos);
     }
 
 }
---
+++ src/controler/Helper/AgendaHelper.java
@@ -0,0 +1,18 @@
+package controler.Helper;
+
+import barbershop.View.Agenda;
+import java.util.ArrayList;
+import model.Agendamento;
+
+public class AgendaHelper {
+    private final Agenda view;
+
+    public AgendaHelper(Agenda view) {
+        this.view = view;
+    }
+
+    public void preencherTabela(ArrayList<Agendamento> agendamentos) {
+        
+    }
+    
+}
